f93dec0728047e9f1ca4d59fb4a61573cea72fd4
Annie <gichiraanne@gmail.com>
2019-11-24 22:44:45 +0300

    Fix more errors and comment code

--- stats/attendance.py
+++ stats/attendance.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 #import games dataframe
-from data.py import games
+from data import games
 
 #select attendance
-attendance = games.loc[:,'year' == 'info','multi3' == 'attendance']
+#attendance = games.loc[:,['year' == 'info','multi3' == 'attendance']]
+attendance = games.loc[:, ['year','multi3']]
 
 #change columns
 attendance.columns = ['year','attendance']
 
 #convert to numeric
-attendance.loc[:, 'column'] = pd.to_numeric(attendance.loc[:, 'column'])
+#attendance.loc[:, 'column'] = pd.to_numeric(attendance.loc[:, 'column'])
 
 #plot graph
-attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
+#attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
 
 #add labels
-plt.xlabel('Year')
-plt.ylabel('Attendance')
+#plt.xlabel('Year')
+#plt.ylabel('Attendance')
 
-plt.show()
+#plt.show()
 
 #draw the mean line
 #plt.axhline(attendance['attendance'].mean()): 'Mean', 'dashed', 'green'
